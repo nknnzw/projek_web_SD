ed17671558a4b598b54be624da239860602c307a
nknnzw <nikennazwa5@gmail.com>
2024-06-03 11:39:42 +0700

    progress

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,35 +19,38 @@ class AuthController extends Controller
     }
     public function authenticate(Request $request)
     {
-   $credentials = $request->validate([
-   'email' => ['required', 'email'],
-   'password' => ['required'],
-    ]);
-   if (Auth::attempt($credentials)) {
-   $request->session()->regenerate();
-   return redirect()->intended('/');
- }
-return back()->withErrors([
-'email' => 'The provided credentials do not match our records.',
- ]);
-} 
-       public function store(Request $request){
-        $validate = $request->validate([
-        'name' => 'required',
-        'email' => 'required|unique:users,email',
-        'password' => 'required',
-        'confirm-password' => 'required|same:password'
-         ]);
-        $data = $request->except('confirm-password', 'password');
-        $data['password'] = Hash::make($request->password);
-        User::create($data);
-        return redirect('/login');}
-    public function logout(Request $request)
-        {
-       Auth::logout();
-       $request->session()->invalidate();
-       $request->session()->regenerateToken();
-       return redirect('/login');
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dash');
         }
-       
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required',
+            'confirm-password' => 'required|same:password'
+        ]);
+        $data = $validate;
+        unset($data["password"], $data["confirm-password"]);
+        $data['password'] = Hash::make($validate['password']);
+        User::create($data);
+        return redirect('/login');
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
 }

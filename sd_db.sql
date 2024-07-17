8a2149154d3197e60768d854ad83a18b53b9b249
nknnzw <nikennazwa5@gmail.com>
2024-07-17 20:03:29 +0700

    progres

--- sd_db.sql
+++ sd_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2024 at 04:57 AM
+-- Generation Time: Jul 17, 2024 at 03:01 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -57,7 +57,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_06_24_034534_tabel_guru', 2);
 
 -- --------------------------------------------------------
 
@@ -107,6 +108,50 @@ CREATE TABLE `tabel_gr` (
   `status_kepegawaian` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `tabel_gr`
+--
+
+INSERT INTO `tabel_gr` (`id`, `gambar`, `nama`, `nip`, `tanggal_lahir`, `jenis_kelamin`, `jabatan`, `status_kepegawaian`) VALUES
+(1, 'C:\\xampp\\tmp\\phpE50.tmp', 'gm12', 123, '2024-06-24', 'Laki-laki', 'ij', 'tj');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_guru`
+--
+
+CREATE TABLE `tabel_guru` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) NOT NULL,
+  `nip` varchar(255) NOT NULL,
+  `tanggal_lahir` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `status_kepegawaian` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tabel_guru`
+--
+
+INSERT INTO `tabel_guru` (`id`, `foto`, `nama`, `nip`, `tanggal_lahir`, `jenis_kelamin`, `jabatan`, `status_kepegawaian`, `created_at`, `updated_at`) VALUES
+(9, 'x3c4Y2RW0ET58ExfOtKoAdFQHqvqueh5N6Yvd2Rq.jpg', 'KASAN, S.Pd.SD', '196409281988061001', '1964-09-28', 'Laki-laki', 'Kepala Sekolah', 'PNS', NULL, NULL),
+(12, 'sx0eviR37hiMWcXoOuDxI7Z3PVXeaq7Ecki7ll09.jpg', 'NANIK ZULAEKAH, S.Pd', '198512312024212042', '1985-12-31', 'Perempuan', 'Wali Kelas 1', 'PPPK', NULL, NULL),
+(13, 'gDjOUvRso6KmxtKw9Y39nDi6KZflgPGOwHMEb0td.jpg', 'SITI MAIMUNAH FEBRIANI, S.Pd', '200002242024212010', '2000-02-24', 'Perempuan', 'Wali Kelas 2', 'PPPK', NULL, NULL),
+(14, 'vR2Rodoev00ApQf4xGXPDJOpS4vRF5G0dRyhHVLz.jpg', 'ATI’ INDAH KUMALASARI, S.Pd', '199811252024212023', '1998-11-25', 'Perempuan', 'Wali Kelas 3', 'PPPK', NULL, NULL),
+(15, 'GAcdwuPKqtqei0pfu2ZrTAznYe3OSMHwCRAzAbdc.jpg', 'ABDUL WAHID, S.Pd.SD.', '198508082022211023', '1985-08-08', 'Laki-laki', 'Wali Kelas 4', 'PPPK', NULL, NULL),
+(16, 'gDClTK4G7zqXcnGoDc9cXfjZ2AIhuPkE2xBLUopS.jpg', 'M MIFTAHUL HUDA, S.Pd.SD.', '198705232022211010', '1987-05-23', 'Laki-laki', 'Wali Kelas 5', 'PPPK', NULL, NULL),
+(17, 'Cce9hq2DPs2XK3df5t6J9AoNlqEe1TPOjaivUvmK.png', 'ANIKH MISHBAH K., S.Pd.', '197705141999012001', '1977-05-14', 'Perempuan', 'Wali Kelas 6', 'PNS', NULL, NULL),
+(18, 'g1rJYBvCoZzFpfBNSlluXcRvuJt5Qjz4LHW0c1g4.jpg', 'ENDAH WAHYUNINGSIH, S.Pd.I.', '198310142014062002', '1983-10-14', 'Perempuan', 'Guru PAI', 'PNS', NULL, NULL),
+(19, 'FQniWlB5WePMuVnq1Ub1VwmQdaobBSmPaEHoeFm6.jpg', 'MUHAMMAD RIZQI, S.Pd', '-', '1999-09-14', 'Laki-laki', 'Guru PJOK', '-', NULL, NULL),
+(20, 'yWltUnXjjjpgzBFS3oMJJli2J3W23oRW9qesfilo.jpg', 'SARIROH SABILMA, S.Pd', '-', '1998-03-11', 'Perempuan', 'Guru Mulok', 'GTT', NULL, NULL),
+(21, 'UdJ1sE8au4ImvBuuI2xpPv0M3SOzZ6Db4BbbaSDf.jpg', 'ZULFAA MUYASSAROH, S.Pd', '-', '1999-10-22', 'Perempuan', 'Guru Mulok', 'GTT', NULL, NULL),
+(22, 'weysKg2TgY3LX6DtUjd0N2K2ydnPzhScqrKbtQT0.jpg', 'Ma’ruf, SE.', '-', '1977-08-06', 'Laki-laki', 'Guru Bahasa Inggris', 'GTT', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -130,7 +175,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'nik', 'niken@gmail.com', NULL, '$2y$10$nLka9sYssBkJeYON.Oxl6OVJjBZww5KCem3hF7AqXru2eIlzaTotm', NULL, '2024-06-02 21:07:04', '2024-06-02 21:07:04'),
-(2, 'niken nazwa', 'nikennazwa@gmail.com', NULL, '$2y$10$L7xeCC5l630gs23plHxovOx0wwPghlI5GM1U/s46mcSbkxI2.cz/e', NULL, '2024-06-02 21:09:17', '2024-06-02 21:09:17');
+(2, 'niken nazwa', 'nikennazwa@gmail.com', NULL, '$2y$10$L7xeCC5l630gs23plHxovOx0wwPghlI5GM1U/s46mcSbkxI2.cz/e', NULL, '2024-06-02 21:09:17', '2024-06-02 21:09:17'),
+(3, 'babi', 'babi@gmail.com', NULL, '12345', NULL, NULL, NULL),
+(4, 'nik', 'nikk@gmail.com', NULL, '$2y$10$v5HhIESfkdNo7cdo7QWsneHloLfSTTYdmTt3vw2RRn/gmla2rX116', NULL, '2024-06-25 09:25:50', '2024-06-25 09:25:50'),
+(5, 'nazwa', 'nazwa@gmail.com', NULL, '$2y$10$LPma0LbCVF5EzYKUfebfTOn7GEz7k.bWFRiVh08ZAVTRTrujHQ5AO', NULL, '2024-07-14 20:35:23', '2024-07-14 20:35:23');
 
 --
 -- Indexes for dumped tables
@@ -164,6 +212,12 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `tabel_guru`
+--
+ALTER TABLE `tabel_guru`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -184,7 +238,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -193,10 +247,16 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tabel_guru`
+--
+ALTER TABLE `tabel_guru`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
